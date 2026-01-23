fb0d789af5542c9cc4225d30875d891dcabf0d00
Sid <sample1561@gmail.com>
2026-01-23 20:49:44 +0530

    Javadocs

--- src/main/java/edu/pec/dromeas/service/LanguageService.java
+++ src/main/java/edu/pec/dromeas/service/LanguageService.java
@@ -4,8 +4,20 @@ import edu.pec.dromeas.config.Language;
 import edu.pec.dromeas.exception.ServerException;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for handling operations related to programming languages,
+ * such as retrieving file extensions and execution commands.
+ */
 @Service
 public class LanguageService {
+
+  /**
+   * Returns the standard file extension for a given programming language.
+   *
+   * @param language The programming language
+   * @return String representing the file extension (including the dot, e.g., ".py")
+   * @throws ServerException if the language is unknown or unsupported
+   */
   public String getExtension(Language language) {
     switch (language) {
       case C:
@@ -32,6 +44,13 @@ public class LanguageService {
     }
   }
 
+  /**
+   * Returns the command used to execute code for a given programming language.
+   *
+   * @param language The programming language
+   * @return String representing the command to run code in that language
+   * @throws ServerException if the language is unknown or unsupported
+   */
   public String getCommand(Language language) {
     switch (language) {
       case C:

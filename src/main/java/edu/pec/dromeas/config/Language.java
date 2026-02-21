5b0de56a51debb3423d5ed936bef3fc636396a10
Sid <sample1561@gmail.com>
2026-02-21 22:52:12 +0530

    Spotless

--- src/main/java/edu/pec/dromeas/config/Language.java
+++ src/main/java/edu/pec/dromeas/config/Language.java
@@ -1,8 +1,6 @@
 package edu.pec.dromeas.config;
 
-/**
- * Enum representing different programming languages supported.
- */
+/** Enum representing different programming languages supported. */
 public enum Language {
   C,
   CPP,
